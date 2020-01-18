e4fffbcdb21aff009257868a717c4309b3324bf6
Chengyu Ko <kokevin678@gmail.com>
2020-01-17 22:55:06 -0800

    add employee fuction working

--- server.js
+++ server.js
@@ -81,10 +81,13 @@ function allEmployee() {
 
 // Search Department
 function byDepartment() {
-  var query = "SELECT * FROM department";
+  var query =
+    "SELECT * FROM department INNER JOIN role ON role.department_id = department.id INNER JOIN employee ON employee.role_id = role.id ";
   connection.query(query, function(err, results) {
     const table = cTable.getTable(results);
     console.log(table);
+    if (err) throw err;
+    runSearch();
   });
 }
 
@@ -92,7 +95,48 @@ function byDepartment() {
 function byManager() {}
 
 // Add Employee
-function addEmployee() {}
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the employee's first name?"
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the employee's last name?"
+      },
+      {
+        name: "role_id",
+        type: "rawlist",
+        choices: [
+          { name: "Sales Lead", value: 1 },
+          { name: "Salesperson", value: 2 },
+          { name: "Lead Engineer", value: 3 },
+          { name: "Software Engineer", value: 4 },
+          { name: "Accountant", value: 5 },
+          { name: "Legal Team Lead", value: 6 },
+          { name: "Lawyer", value: 7 }
+        ]
+      }
+    ])
+    .then(function(answer) {
+      var firstName = answer.first_name;
+      var lastName = answer.last_name;
+      var role = answer.role_id;
+      var query =
+        "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?); ";
+      connection.query(query, [firstName, lastName, role], function(
+        err,
+        results
+      ) {
+        console.log("added");
+        runSearch();
+      });
+    });
+}
 
 // Remove Employee
 function removeEmployee() {}
